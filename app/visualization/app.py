009d3fe6e2c160caf5adf7c227f973165a4e08eb
prayudahlah <yudafihan@gmail.com>
2026-06-24 12:55:47 +0700

    feat: synchronize streamlit with real data

--- app/visualization/app.py
+++ app/visualization/app.py
@@ -3,7 +3,10 @@ from datetime import date, timedelta
 from streamlit_autorefresh import st_autorefresh
 from config import (
     PAGE_SIZE,
-    ASUS_BLUE, BG_BORDER, TEXT_PRIMARY, TEXT_SECONDARY,
+    ASUS_BLUE,
+    BG_BORDER,
+    TEXT_PRIMARY,
+    TEXT_SECONDARY,
 )
 from components import (
     inject_css,
@@ -40,7 +43,7 @@ inject_css()
 st_autorefresh(interval=5000)
 
 default_from = date.today() - timedelta(days=30)
-default_to   = date.today()
+default_to = date.today()
 
 if "filter_date_from" not in st.session_state:
     st.session_state.filter_date_from = default_from
@@ -61,15 +64,15 @@ text-transform:uppercase;color:{TEXT_SECONDARY};">ASUS Indonesia</div>
     )
 
     st.markdown("**Rentang**")
-    if st.button("Hari Ini", width='stretch', key="btn_today"):
+    if st.button("Hari Ini", width="stretch", key="btn_today"):
         st.session_state.filter_date_from = date.today()
         st.session_state.filter_date_to = date.today()
         st.rerun()
-    if st.button("7 Hari", width='stretch', key="btn_7d"):
+    if st.button("7 Hari", width="stretch", key="btn_7d"):
         st.session_state.filter_date_from = date.today() - timedelta(days=7)
         st.session_state.filter_date_to = date.today()
         st.rerun()
-    if st.button("30 Hari", width='stretch', key="btn_30d"):
+    if st.button("30 Hari", width="stretch", key="btn_30d"):
         st.session_state.filter_date_from = date.today() - timedelta(days=30)
         st.session_state.filter_date_to = date.today()
         st.rerun()
@@ -85,10 +88,12 @@ text-transform:uppercase;color:{TEXT_SECONDARY};">ASUS Indonesia</div>
     st.divider()
 
     product_options = get_products()
-    st.selectbox("Produk",
-                  options=[ALL_PRODUCT, *product_options],
-                  format_func=lambda x: "Semua Produk" if x == ALL_PRODUCT else x,
-                  key="filter_product")
+    st.selectbox(
+        "Produk",
+        options=[ALL_PRODUCT, *product_options],
+        format_func=lambda x: "Semua Produk" if x == ALL_PRODUCT else x,
+        key="filter_product",
+    )
 
     st.divider()
 
@@ -102,7 +107,7 @@ text-transform:uppercase;color:{TEXT_SECONDARY};">ASUS Indonesia</div>
 
     st.divider()
 
-    if st.button("Reset Filter", width='stretch', key="btn_reset"):
+    if st.button("Reset Filter", width="stretch", key="btn_reset"):
         st.session_state.filter_date_from = default_from
         st.session_state.filter_date_to = default_to
         st.session_state.filter_product = ALL_PRODUCT
@@ -129,7 +134,7 @@ if "alert_page" not in st.session_state:
 filter_key = str(filters)
 if st.session_state.get("_last_filter_key") != filter_key:
     st.session_state.review_page = 0
-    st.session_state.alert_page  = 0
+    st.session_state.alert_page = 0
     st.session_state["_last_filter_key"] = filter_key
 
 render_header()
@@ -147,7 +152,7 @@ render_time_series(df_ts)
 st.markdown("<hr class='section-divider'>", unsafe_allow_html=True)
 
 df_sent = get_sentiment_distribution(filters)
-df_emo  = get_emotion_distribution(filters)
+df_emo = get_emotion_distribution(filters)
 render_sentiment_emotion(df_sent, df_emo)
 
 st.markdown("<hr class='section-divider'>", unsafe_allow_html=True)
@@ -158,8 +163,9 @@ render_top_products(df_products)
 st.markdown("<hr class='section-divider'>", unsafe_allow_html=True)
 
 total_reviews = get_total_reviews_count(filters)
-df_reviews    = get_reviews_page(
-    filters, limit=PAGE_SIZE,
+df_reviews = get_reviews_page(
+    filters,
+    limit=PAGE_SIZE,
     offset=st.session_state.review_page * PAGE_SIZE,
 )
 render_reviews_table(df_reviews, st.session_state.review_page, total_reviews, PAGE_SIZE)
@@ -167,9 +173,9 @@ render_reviews_table(df_reviews, st.session_state.review_page, total_reviews, PA
 st.markdown("<hr class='section-divider'>", unsafe_allow_html=True)
 
 total_alerts = get_total_alerts_count(filters)
-df_alerts    = get_alerts_page(
-    filters, limit=PAGE_SIZE,
+df_alerts = get_alerts_page(
+    filters,
+    limit=PAGE_SIZE,
     offset=st.session_state.alert_page * PAGE_SIZE,
 )
 render_alerts_table(df_alerts, st.session_state.alert_page, total_alerts, PAGE_SIZE)
-
